966c0083237783bbe8b6b4da3fa4a78909e454ef
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-12-27 01:08:09 +0800

    version 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
 import './App.scss';
+// import favourite from '../src/img/favourite.svg';
 
 const App = () => (
   <div className="App">
-    <h1>React Phone Catalog</h1>
+    <header className="header">
+      <div className="header__left">
+        <a href="/" className="logo">
+          <img
+            src={require('./img/LOGO.svg').default}
+            alt="logo"
+            // className="loo"
+          />
+        </a>
+        <nav className="nav">
+          <li className="nav__item"><a href="/">home</a></li>
+          <li className="nav__item"><a href="/">Phones</a></li>
+          <li className="nav__item"><a href="/">tablets</a></li>
+          <li className="nav__item"><a href="/">accessories</a></li>
+        </nav>
+      </div>
+
+      <div className="header__right">
+        <a href="/" className="header__right-icon">
+          <img
+            src={require('./img/favourite.svg').default}
+            alt="favourite"
+            className="favourite icons"
+          />
+        </a>
+        <a href="/" className="header__right-icon">
+          <img
+            src={require('./img/cart.svg').default}
+            alt="cart"
+            className="cart icons"
+          />
+        </a>
+      </div>
+    </header>
   </div>
 );
 
